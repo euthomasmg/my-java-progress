9ccf652fd550deb50f825c9313304cdd5e4fe599
Thomas <thomas2003.mayer@gmail.com>
2025-01-14 23:22:30 -0300

    file temperature

---
+++ exercises/src/fundamentals/Inference.java
@@ -0,0 +1,9 @@
+package fundamentals;
+
+public class Inference {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+    }
+}
--- exercises/src/fundamentals/Temperature.java
+++ exercises/src/fundamentals/Temperature.java
@@ -1,4 +1,22 @@
 package fundamentals;
 
 public class Temperature {
+
+    public static void main(String[] args){
+
+        //(F° - 32) x 5 / 9 = C°
+
+        final double AJUSTE = 32;
+        final double FATOR = 5 / 9.0; //o .0 depois do nove, o Java entende que voce esta trabalhando com números quebrados.
+        double f = 86;
+        double c = (f - AJUSTE) * FATOR;
+
+        System.out.println(c);
+
+        f = 9;
+        c = (f - AJUSTE) * FATOR;
+
+        System.out.println(c);
+
+    }
 }
