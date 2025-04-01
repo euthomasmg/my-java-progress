c9a6f4fd1cf27d87fbdde88c2885df861e910d8a
Thomas <thomas2003.mayer@gmail.com>
2025-03-31 22:36:32 -0300

    switch sem brack

---
+++ exercises/src/control/ChallnegeFor.java
@@ -0,0 +1,17 @@
+package control;
+
+public class ChallnegeFor {
+
+    public static void main(String[] args) {
+
+        String value = "#";
+
+        for (int i = 1; i <= 5; i++){
+
+            System.out.println(value);
+
+            value += "#";
+
+        }
+    }
+}
---
+++ exercises/src/control/SwitchSemBrack.java
@@ -0,0 +1,28 @@
+package control;
+
+public class SwitchSemBrack {
+
+    public static void main(String[] args) {
+
+        String faixa = "preta";
+
+        switch (faixa.toLowerCase()) {
+
+            case "preta":
+
+                System.out.println("Sabe faze uma luta foda");
+
+            case "marrom":
+
+                System.out.println("Uns negoico la da faixa marrom");
+
+            case "verde":
+
+                System.out.println("uns bglh la e fds");
+
+            default:
+
+                System.out.println("nn sei fazer nada!");
+        }
+    }
+}
