5acc20d26d7414edce6254ec2bec5b64b463d17b
Thomas <thomas2003.mayer@gmail.com>
2025-05-08 21:54:22 -0300

    chore: Challnege 3 and 4

--- exercises/src/control/challenge/Challenge4.java
+++ exercises/src/control/challenge/Challenge4.java
@@ -3,30 +3,35 @@ package control.challenge;
 import java.util.Scanner;
 
 public class Challenge4 {
-    
+
     public static void main(String[] args) {
 
-        //1. Criar um programa que receba um número e verifique se ele está entre 0 e 10 e é par;
+        /*
+        Criar um programa que receba um número e diga se ele é um número primo.
+         */
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Informe um valor: ");
-        double value = input.nextDouble();
+        System.out.print("Informe um número inteiro: ");
+        int n1 = input.nextInt();
 
-        if (value >= 0 && value <= 10){
+        int contadorDeDivisores = 0;
 
-            if (value % 2 == 0) {
+        for (int i = 1; i <= n1; i++) {
 
-                System.out.print("Número esta entre 0 e 10 e é par");
-            } else {
+            if (n1 % i == 0) {
 
-                System.out.print("Número esta entre 0 e 10 e não é par");
+                contadorDeDivisores++;
             }
         }
-        else {
 
-            System.out.println("Número informado não esta entre 0 e 10 e não é par");
-
+        if (n1 < 2 || contadorDeDivisores > 2) {
+            System.out.println("Não é primo!");
+        } else {
+            System.out.println("É primo!");
         }
+
+        input.close();
+
     }
 }
